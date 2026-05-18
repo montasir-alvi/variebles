08f14b89955318d5c2c25697777464b35122a8d2
Montasir Alvi <montasirc727@gmail.com>
2026-05-18 07:24:09 +0000

    Day 1 - Variables and swap logic

---
+++ day1_variables.py
@@ -0,0 +1,30 @@
+montasir_bank_balance = 10000
+his_wife_bank_balance = 5000
+#they swap there debit cards
+temp = montasir_bank_balance
+montasir_bank_balance = his_wife_bank_balance
+his_wife_bank_balance = temp
+print("Montasir has",montasir_bank_balance,"in his debit card")
+print("His wife has",his_wife_bank_balance,"in her debit card")
+
+#new code start
+car = "Mercedes"
+cost = 350000
+city = "california"
+print("Montasir has a",car,"in his",city,"apartment. which cost",cost,"Dollars")
+
+#new code start
+Montasir_have = 20
+Albert_have = 15
+temp = Montasir_have
+Montasir_have = Albert_have
+Albert_have = temp
+print("Montasir has",Albert_have,"million dollars. After buying Albert's apartment, now he have",Montasir_have,"million dollars in his account")
+
+#new code start
+sakib = 75
+tamim = 92
+temp = sakib
+sakib = tamim
+tamim = temp
+print("In this next match, Sakib will target",sakib,"runs and Tamim will target",tamim,"runs")
